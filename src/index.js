ac541f06d618e4a04bab76ef4f07277ea514759d
chazzlee <chazz@dev.com>
2022-11-16 18:10:25 -0500

    add playback feature

--- src/index.js
+++ src/index.js
@@ -40,11 +40,25 @@ export const DEFAULT_SAMPLE_PACK = {
   7: { label: "Kick 1", name: "kick-1", path: "./assets/s1/Kicks/kick-5.wav" },
 };
 
+function setBpm(value) {
+  const $bpmValue = $("#bpm-value");
+  $bpmValue.textContent = value;
+}
+
+//TODO: max min default start (ui)
+function setVolume(value) {
+  const $volumeValue = $("#volume-value");
+  $volumeValue.textContent = value;
+}
+
 function bootstrap() {
   $("#sequencer-container").addEventListener("contextmenu", (e) => {
     e.preventDefault();
     return false;
   });
+
+  setBpm(Tone.Transport.bpm.value);
+  setVolume(Tone.Destination.volume.value);
 }
 bootstrap();
 
@@ -55,6 +69,10 @@ function createPlayersFromSamples(samples) {
 }
 
 const players = createPlayersFromSamples(DEFAULT_SAMPLE_PACK);
+const metronome = new Tone.MembraneSynth({
+  volume: -8,
+  envelope: { attack: 0.05 },
+}).toDestination();
 
 function playNote(source, time, note = "C4", duration = "8n") {
   if (source instanceof Tone.Player) {
@@ -63,6 +81,12 @@ function playNote(source, time, note = "C4", duration = "8n") {
     source.triggerAttackRelease(note, duration, time);
   }
 }
+
+function createLooper(count = 16, value = null) {
+  return new Array(count).fill(value);
+}
+
+//--------------------------------------------------------------//
 
 // DOM Elements
 
@@ -79,8 +103,8 @@ $sideKeyboardKeys.forEach(($key, index) => {
   });
 });
 
-function extractRowIndex(row) {
-  return parseInt(row.split("_")[1], 10);
+function extractPosition(position) {
+  return JSON.parse(position);
 }
 
 function placeMarker($cell, row) {
@@ -95,13 +119,63 @@ function hasMarker($cell) {
   return $cell.hasChildNodes();
 }
 
+function createLoopManager(players) {
+  let loopers = players.map((_player) => createLooper());
+  let sequences = {};
+
+  return {
+    updateLoop(position, note = "C4") {
+      let [row, col] = position;
+      loopers[row][col] = note;
+    },
+    resetLoops() {
+      loopers = players.map((_player) => createLooper());
+    },
+    createSequences() {
+      players.forEach((player, index) => {
+        if (sequences[index]) {
+          sequences[index].dispose();
+        }
+        sequences[index] = new Tone.Sequence((time, note) => {
+          playNote(player, time, note);
+        }, loopers[index]).start(0);
+      });
+    },
+    getSequences() {
+      return sequences;
+    },
+    start() {
+      Tone.Transport.start();
+    },
+  };
+}
+
+const $sequencerGrid = $("#sequencer-grid");
+const $cells = Array.from($sequencerGrid.children);
+
+const loopManager = createLoopManager(players);
+
+const $playButton = $("#btn-play");
+$playButton.addEventListener("click", () => {
+  if (Tone.Transport.state !== "started") {
+    loopManager.createSequences();
+    loopManager.start();
+  }
+});
+
+const $stopButton = $("#btn-stop");
+$stopButton.addEventListener("click", () => {
+  if (Tone.Transport.state === "started") {
+    Tone.Transport.stop();
+  }
+});
+
 // Grid
 const LEFT_BUTTON = 0;
 const RIGHT_BUTTON = 2;
-const $sequencerGrid = $("#sequencer-grid");
-const $cells = Array.from($sequencerGrid.children);
+
 $cells.forEach(($cell) => {
-  let row = extractRowIndex($cell.dataset.note);
+  let [row, col] = extractPosition($cell.dataset.pos);
   $cell.dataset.name = DEFAULT_SAMPLE_PACK[row].name;
 
   $cell.addEventListener("mousedown", (e) => {
@@ -111,11 +185,54 @@ $cells.forEach(($cell) => {
 
       if (!hasMarker($cell)) {
         placeMarker($cell, row);
+        loopManager.updateLoop([row, col]);
       }
     } else if (e.button === RIGHT_BUTTON && hasMarker($cell)) {
       removeMarker($cell);
+      loopManager.updateLoop([row, col], null);
     }
   });
 });
 
 // BPM Slider
+const $bpmSlider = $("#bpm-slider");
+$bpmSlider.addEventListener("input", (e) => {
+  const value = parseInt(e.target.value, 10);
+  Tone.Transport.bpm.set({ value });
+  setBpm(value);
+});
+
+const $volumeSlider = $("#volume-slider");
+$volumeSlider.addEventListener("input", (e) => {
+  const value = parseInt(e.target.value, 10);
+  Tone.Destination.volume.set({ value });
+  setVolume(value);
+});
+
+const $clearButton = $("#btn-clear");
+$clearButton.addEventListener("click", () => {
+  const $cells = Array.from($("#sequencer-grid").children);
+  $cells.forEach(($cell) => {
+    if ($cell.hasChildNodes()) {
+      $cell.removeChild($cell.lastElementChild);
+    }
+  });
+  loopManager.resetLoops();
+});
+
+const $muteButton = $("#btn-mute");
+const $unmuteButton = $("#btn-unmute");
+$muteButton.addEventListener("click", () => {
+  if (!Tone.Destination.mute) {
+    $muteButton.classList.add("hide");
+    $unmuteButton.classList.remove("hide");
+    Tone.Destination.mute = true;
+  }
+});
+$unmuteButton.addEventListener("click", () => {
+  if (Tone.Destination.mute) {
+    $unmuteButton.classList.add("hide");
+    $muteButton.classList.remove("hide");
+    Tone.Destination.mute = false;
+  }
+});
