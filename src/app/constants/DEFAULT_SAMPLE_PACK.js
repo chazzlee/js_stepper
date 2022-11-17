c0b92b966915a720fd22a28d7d8c8dda51049056
chazzlee <chazz@dev.com>
2022-11-17 10:49:57 -0500

    change default sample pack paths

--- src/app/constants/DEFAULT_SAMPLE_PACK.js
+++ src/app/constants/DEFAULT_SAMPLE_PACK.js
@@ -41,37 +41,37 @@ export const DEFAULT_SAMPLE_PACK_2 = {
   0: {
     label: "Open Hat 1",
     name: "openhat-1",
-    path: "../samples/openhat1.wav",
+    path: "../assets/openhat1.wav",
   },
   1: {
     label: "Closed Hat 1",
     name: "closedhat-1",
-    path: "../samples/closedhat1.wav",
+    path: "../assets/closedhat1.wav",
   },
   2: {
     label: "Percussion 1",
     name: "percussion-1",
-    path: "../samples/perc1.wav",
+    path: "../assets/perc1.wav",
   },
   3: {
     label: "Percussion 2",
     name: "percussion-2",
-    path: "../samples/perc2.wav",
+    path: "../assets/perc2.wav",
   },
   4: {
     label: "Riff 1",
     name: "riff-1",
-    path: "../samples/riff1.wav",
+    path: "../assets/riff1.wav",
   },
   5: {
     label: "Snare 1",
     name: "snare-1",
-    path: "../samples/snare1.wav",
+    path: "../assets/snare1.wav",
   },
   6: {
     label: "Snare 2",
     name: "snare-2",
-    path: "../samples/snare2.wav",
+    path: "../assets/snare2.wav",
   },
-  7: { label: "Kick 1", name: "kick-1", path: "../samples/kick.wav" },
+  7: { label: "Kick 1", name: "kick-1", path: "../assets/kick.wav" },
 };
