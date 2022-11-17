7ea036547446c5aadd24c55a0269b38f205a9301
chazzlee <chazz@dev.com>
2022-11-16 21:40:06 -0500

    update styling

---
+++ src/app/constants/DEFAULT_SAMPLE_PACK.js
@@ -0,0 +1,38 @@
+export const DEFAULT_SAMPLE_PACK = {
+  0: {
+    label: "Open Hat 1",
+    name: "openhat-1",
+    path: "./assets/s1/Hihats/open-hihat-1.wav",
+  },
+  1: {
+    label: "Closed Hat 1",
+    name: "closedhat-1",
+    path: "./assets/s1/Hihats/closed-hihat-1.wav",
+  },
+  2: {
+    label: "Percussion 1",
+    name: "percussion-1",
+    path: "./assets/s1/Percussion/percussion-9.wav",
+  },
+  3: {
+    label: "Percussion 2",
+    name: "percussion-2",
+    path: "./assets/s1/Percussion/percussion1.wav",
+  },
+  4: {
+    label: "Percussion 3",
+    name: "percussion-3",
+    path: "./assets/s1/Percussion/Cymatics x S1 - Percussion 4.wav",
+  },
+  5: {
+    label: "Snare 1",
+    name: "snare-1",
+    path: "./assets/s1/Snares/Cymatics x S1 - Snare 6.wav",
+  },
+  6: {
+    label: "Snare 2",
+    name: "snare-2",
+    path: "./assets/s1/Snares/snare8.wav",
+  },
+  7: { label: "Kick 1", name: "kick-1", path: "./assets/s1/Kicks/kick-5.wav" },
+};
--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
 import * as Tone from "tone";
 import { $, $$ } from "./app/utils";
-
-export const DEFAULT_SAMPLE_PACK = {
-  0: {
-    label: "Open Hat 1",
-    name: "openhat-1",
-    path: "./assets/s1/Hihats/open-hihat-1.wav",
-  },
-  1: {
-    label: "Closed Hat 1",
-    name: "closedhat-1",
-    path: "./assets/s1/Hihats/closed-hihat-1.wav",
-  },
-  2: {
-    label: "Percussion 1",
-    name: "percussion-1",
-    path: "./assets/s1/Percussion/percussion-9.wav",
-  },
-  3: {
-    label: "Percussion 2",
-    name: "percussion-2",
-    path: "./assets/s1/Percussion/percussion1.wav",
-  },
-  4: {
-    label: "Percussion 3",
-    name: "percussion-3",
-    path: "./assets/s1/Percussion/Cymatics x S1 - Percussion 4.wav",
-  },
-  5: {
-    label: "Snare 1",
-    name: "snare-1",
-    path: "./assets/s1/Snares/Cymatics x S1 - Snare 6.wav",
-  },
-  6: {
-    label: "Snare 2",
-    name: "snare-2",
-    path: "./assets/s1/Snares/snare8.wav",
-  },
-  7: { label: "Kick 1", name: "kick-1", path: "./assets/s1/Kicks/kick-5.wav" },
-};
+import { DEFAULT_SAMPLE_PACK } from "./app/constants/DEFAULT_SAMPLE_PACK.js";
 
 function setBpm(value) {
   const $bpmValue = $("#bpm-value");
@@ -51,6 +13,11 @@ function setVolume(value) {
 }
 
 function bootstrap() {
+  document.addEventListener("DOMContentLoaded", async () => {
+    await Tone.start();
+    console.log("Audio is ready");
+  });
+
   $("#sequencer-container").addEventListener("contextmenu", (e) => {
     e.preventDefault();
     return false;
